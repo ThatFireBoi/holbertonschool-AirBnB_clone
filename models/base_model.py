90a52449d6d3adcc5efe1440cd3af8dcb6acc043
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-30 14:08:41 -0400

    Update func for to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,6 @@ from uuid import uuid4
 from datetime import datetime
 from models import storage
 import uuid
-import json
 import sys
 import os.path
 
@@ -16,23 +15,13 @@ import os.path
 class BaseModel():
     ''' a base class for other classes '''
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         '''
         initializes the values
         '''
-        if kwargs:
-            dtf = '%Y-%m-%dT%H:%M:%S.%f'
-            k_dict = kwargs.copy()
-            del k_dict["__class__"]
-            for key in k_dict:
-                if ("created_at" == key or "updated_at" == key):
-                    k_dict[key] = datetime.strptime(k_dict[key], dtf)
-            self.__dict__ = k_dict
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         '''
@@ -41,7 +30,7 @@ class BaseModel():
         return ('[{}] ({}) {}'.format(
             self.__class__.__name__,
             self.id,
-            self.__class__.__dict__))
+            self.__dict__))
 
     def save(self):
         '''
@@ -49,30 +38,14 @@ class BaseModel():
         with the current datetime
         '''
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         '''
         returns a dictionary containing all keysvalues
         of __dict__ of the instance
         '''
-        dic = {}
-        dic["__class__"] = self.__class__.__name__
-        for k, v in self.__dict__.items():
-            if isinstance(v, (datetime, )):
-                dic[k] = v.isoformat()
-            else:
-                dic[k] = v
+        dic = self.__dict__.copy()
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
         return dic
-
-    def to_json(self):
-        '''
-        returns a json containing all keysvalues
-        of __dict__ of the instance
-        '''
-        my_json = self.__dict__.copy()
-        my_json.update({'created_at': self.created_at.strftime(self.dtf)})
-        my_json.update({'__class__': str(self.__class__.__name__)})
-        if hasattr(self, 'updated_at'):
-            my_json.update({'updated_at': self.updated_at.strftime(self.dtf)})
-        return my_json
