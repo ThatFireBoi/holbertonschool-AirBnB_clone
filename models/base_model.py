9728beaacdf840d73df5364abd1d380d0a8cb5f7
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-30 15:23:30 -0400

    large

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,20 @@ import uuid
 class BaseModel():
     ''' a base class for other classes '''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
         initializes the values
         '''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         '''
