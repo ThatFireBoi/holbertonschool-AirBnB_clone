530612a796f21d87babf62b3380a581f8d7f072e
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-30 16:22:39 -0400

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,78 +1,33 @@
-#!/usr/bin/python3
-"""
-Module base_model
-Contains a Class that defines all common attributes or
-methods for other classes
-"""
-from uuid import uuid4
-from datetime import datetime
-from models import storage
 import uuid
-import json
-import sys
-import os.path
+from datetime import datetime
+from models import storage  # Import the storage instance
 
 
-class BaseModel():
-    ''' a base class for other classes '''
-
+class BaseModel:
     def __init__(self, *args, **kwargs):
-        '''
-        initializes the values
-        '''
         if kwargs:
-            dtf = '%Y-%m-%dT%H:%M:%S.%f'
-            k_dict = kwargs.copy()
-            del k_dict["__class__"]
-            for key in k_dict:
-                if ("created_at" == key or "updated_at" == key):
-                    k_dict[key] = datetime.strptime(k_dict[key], dtf)
-            self.__dict__ = k_dict
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
         else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-
-    def __str__(self):
-        '''
-        print in "[<class name>] (<self.id>) <self.__dict__>" format
-        '''
-        return ('[{}] ({}) {}'.format(
-            self.__class__.__name__,
-            self.id,
-            self.__class__.__dict__))
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)  # Call new method of the storage instance
 
     def save(self):
-        '''
-        updates the public instance attribute updated_at
-        with the current datetime
-        '''
         self.updated_at = datetime.now()
-        storage.save()
+        storage.save()  # Call save method of the storage instance
 
     def to_dict(self):
-        '''
-        returns a dictionary containing all keysvalues
-        of __dict__ of the instance
-        '''
-        dic = {}
-        dic["__class__"] = self.__class__.__name__
-        for k, v in self.__dict__.items():
-            if isinstance(v, (datetime, )):
-                dic[k] = v.isoformat()
-            else:
-                dic[k] = v
-        return dic
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
 
-    def to_json(self):
-        '''
-        returns a json containing all keysvalues
-        of __dict__ of the instance
-        '''
-        my_json = self.__dict__.copy()
-        my_json.update({'created_at': self.created_at.strftime(self.dtf)})
-        my_json.update({'__class__': str(self.__class__.__name__)})
-        if hasattr(self, 'updated_at'):
-            my_json.update({'updated_at': self.updated_at.strftime(self.dtf)})
-        return my_json
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,54 +1,28 @@
-#!/usr/bin/python3
-"""This module defines a class to manage file storage for the HBnB clone"""
-
-
 import json
-from os import path
-from models.base_model import BaseModel
-from models.city import City
-from models.state import State
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
-from models.user import User
+from os.path import isfile
 
 
 class FileStorage:
-    """This class manages storage of ABnB models in JSON format
-
-    Attributes:
-        __file_path (str): path to JSON file
-        __objects (dict): dictionary of models currently in storage
-    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
-        key = obj.__class__.__name__ + '.' + obj.id
+        key = "{}.{}".format(obj.__class__.____name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
-        new_dict = {}
-        for key, value in self.__objects.items():
-            new_dict[key] = value.to_dict()
-        with open(self.__file_path, 'w', encoding='utf-8') as f:
-            f.write(json.dumps)(new_dict)
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(obj_dict, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects (only if the JSON file
-        (__file_path) exists); otherwise, do nothing. If the file doesn’t
-        exist, no exception should be raised)
-        """
-        if path.exists(self.__file_path):
-            with open(self.__file_path, 'r') as f:
-                new_dict = json.load(f)
-            for key, value in new_dict.items():
-                class_name = value['__class__']
-                del value['__class__']
-                self.new(eval(class_name)(**value))
+        if isfile(self.__file_path):
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    obj = eval(class_name)(**value)
+                    self.__objects[key] = obj
