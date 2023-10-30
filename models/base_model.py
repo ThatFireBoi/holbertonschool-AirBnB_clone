e7b9861a4d67dccb2a0692beb7ebc8ab83ae3eb2
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-30 15:16:10 -0400

    Hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,10 @@ Module base_model
 Contains a Class that defines all common attributes or
 methods for other classes
 """
-from uuid import uuid4
+
+
 from datetime import datetime
-from models import storage
 import uuid
-import json
-import sys
-import os.path
 
 
 class BaseModel():
