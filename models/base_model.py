0fde3d923af913d7c32127ed36631cba7b3f79de
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-30 13:59:00 -0400

    hot fix base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,57 +1,24 @@
 #!/usr/bin/python3
-"""
-Module base_model
-Contains a Class that defines all common attributes or
-methods for other classes
-"""
-from uuid import uuid4
-from datetime import datetime
-from models import storage
 import uuid
-import json
-import sys
-import os.path
+from datetime import datetime
 
 
-class BaseModel():
-    ''' a base class for other classes '''
-
+class BaseModel:
     def __init__(self):
-        '''
-        initializes the values
-        '''
-        self.id = str(uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        storage.new(self)
 
     def __str__(self):
-        '''
-        print in "[<class name>] (<self.id>) <self.__dict__>" format
-        '''
-        return ('[{}] ({}) {}'.format(
-            self.__class__.__name__,
-            self.id,
-            self.__class__.__dict__))
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        '''
-        updates the public instance attribute updated_at
-        with the current datetime
-        '''
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
-        '''
-        returns a dictionary containing all keysvalues
-        of __dict__ of the instance
-        '''
-        dic = {}
-        dic["__class__"] = self.__class__.__name__
-        for k, v in self.__dict__.items():
-            if isinstance(v, (datetime, )):
-                dic[k] = v.isoformat()
-            else:
-                dic[k] = v
-        return dic
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
