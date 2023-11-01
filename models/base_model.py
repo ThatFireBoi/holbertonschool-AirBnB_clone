eff6c6bb51b29c1979a93dba4d1bc878dc688d43
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-31 20:15:09 -0400

    updated base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,78 @@
-#!/bin/usr/python3
-"""This module defines a class BaseModel"""
-import uuid
+#!/usr/bin/python3
+"""
+Module base_model
+Contains a Class that defines all common attributes or
+methods for other classes
+"""
+from uuid import uuid4
 from datetime import datetime
 from models import storage
+import uuid
+import json
+import sys
+import os.path
 
 
 class BaseModel():
-    """This class defines all common attributes/methods for other classes"""
+    ''' a base class for other classes '''
 
     def __init__(self, *args, **kwargs):
-        """This method initializes a BaseModel instance
-        Args:
-            *args: unused
-            **kwargs: key/value pairs used to create instance
-        attributes:
-            id: unique id for each instance
-            created_at: creation date
-            updated_at: date of last update
-            storage: instance of FileStorage class"""
+        '''
+        initializes the values
+        '''
         if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key != "__class__":
-                    setattr(self, key, value)
-            self.id = kwargs["id"]
+            dtf = '%Y-%m-%dT%H:%M:%S.%f'
+            k_dict = kwargs.copy()
+            del k_dict["__class__"]
+            for key in k_dict:
+                if ("created_at" == key or "updated_at" == key):
+                    k_dict[key] = datetime.strptime(k_dict[key], dtf)
+            self.__dict__ = k_dict
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
-        """This method returns a string representation of a BaseModel
-        instance"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        '''
+        print in "[<class name>] (<self.id>) <self.__dict__>" format
+        '''
+        return ('[{}] ({}) {}'.format(
+            self.__class__.__name__,
+            self.id,
+            self.__class__.__dict__))
 
     def save(self):
-        """This method updates the public instance attribute updated_at
-        with the current datetime when the object is saved"""
+        '''
+        updates the public instance attribute updated_at
+        with the current datetime
+        '''
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """This method returns a dictionary containing all keys/values
-        of __dict__ of the instance"""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        '''
+        returns a dictionary containing all keysvalues
+        of __dict__ of the instance
+        '''
+        dic = {}
+        dic["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if isinstance(v, (datetime, )):
+                dic[k] = v.isoformat()
+            else:
+                dic[k] = v
+        return dic
+
+    def to_json(self):
+        '''
+        returns a json containing all keysvalues
+        of __dict__ of the instance
+        '''
+        my_json = self.__dict__.copy()
+        my_json.update({'created_at': self.created_at.strftime(self.dtf)})
+        my_json.update({'__class__': str(self.__class__.__name__)})
+        if hasattr(self, 'updated_at'):
+            my_json.update({'updated_at': self.updated_at.strftime(self.dtf)})
+        return my_json
