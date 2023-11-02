3ba0894f726a9790e532af3f259ce6883f5f4b8d
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-02 12:31:21 -0400

    Add egde cases

--- tests/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-def test_attribute_assignment(self):
-    """ test attribute assignment """
-    self.my_state.name = "California"
-    self.assertEqual(self.my_state.name, "California")
